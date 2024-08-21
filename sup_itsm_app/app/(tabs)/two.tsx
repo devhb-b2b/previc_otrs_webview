de35baf4ba4a47943b04b8f66b5d500e3edc56e7
devhb <helberbrito@gmail.com>
2024-08-21 15:24:32 -0300

    UPDATE/tabs: two; delete content;

--- sup_itsm_app/app/(tabs)/two.tsx
+++ sup_itsm_app/app/(tabs)/two.tsx
@@ -4,13 +4,14 @@ import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
 export default function TabTwoScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
-    </View>
-  );
+  return null;
+  // return (
+  //   <View style={styles.container}>
+  //     <Text style={styles.title}>Tab Two</Text>
+  //     <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+  //     <EditScreenInfo path="app/(tabs)/two.tsx" />
+  //   </View>
+  // );
 }
 
 const styles = StyleSheet.create({
